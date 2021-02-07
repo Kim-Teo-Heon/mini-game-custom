8abf86ca5e313092dc508bdc31bf2e7b67d6cc59
Kim-Teo-Heon <xbox1720@naver.com>
2021-02-07 22:18:37 +0900

    완료 : 1. li 클릭 시 흔들림 효과 구현

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const clothes = [];
 const lists_ul = document.querySelector('.lists');
 
+function shake(e){
+    const li = e.currentTarget;
+    //currentTarget은 내가 이벤트를 발생시키도록 지정 한 곳(li)
+
+    li.classList.add('shake_li');
+
+    setTimeout(()=>{
+        li.classList.remove('shake_li');
+    }, 1000)
+}
+
 function print_clothes(clothes){
     const li = document.createElement('li');
     const img = document.createElement('img');
@@ -9,6 +20,7 @@ function print_clothes(clothes){
 
     img.src = src;
     span.innerText = `${clothes.sex}, ${clothes.size}`
+    li.addEventListener('click',shake);
 
     li.appendChild(img);
     li.appendChild(span);
